d495e82fd06dc218f315bff03bb8549b6bd64656
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-09-12 16:21:08 +0900

    a

---
+++ src/main/kotlin/com/map/map/MapApplication.kt
@@ -0,0 +1,11 @@
+package com.map.map
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapApplication
+
+fun main(args: Array<String>) {
+    runApplication<MapApplication>(*args)
+}
