a3f5258db085a1e3c18b00df2c78cbb20b5f46ab
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-09-15 17:26:23 +0900

    [add] login service

--- src/main/kotlin/com/map/map/service/auth/AuthServiceImpl.kt
+++ src/main/kotlin/com/map/map/service/auth/AuthServiceImpl.kt
@@ -1,9 +1,15 @@
 package com.map.map.service.auth
 
+import com.map.map.domain.dto.auth.LoginDto
 import com.map.map.domain.dto.auth.RegisterDto
 import com.map.map.domain.entity.User
 import com.map.map.domain.repository.UserRepo
+import com.map.map.domain.response.auth.LoginRo
+import com.map.map.enum.JwtType
 import com.map.map.lib.Crypto
+import com.map.map.service.jwt.JwtService
+import com.map.map.service.jwt.JwtServiceImpl
+import com.sun.org.apache.xpath.internal.operations.Bool
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -12,35 +18,64 @@ import org.springframework.web.client.HttpClientErrorException
 
 @Service
 class AuthServiceImpl @Autowired constructor(
-    private val crypto : Crypto,
-    private val userRepo: UserRepo
-): AuthService {
+    private val crypto: Crypto,
+    private val userRepo: UserRepo,
+    private val jwtService: JwtServiceImpl
+) : AuthService {
 
     @Transactional
-    override fun register(registerDto: RegisterDto){
-        try{
-            var user:User = User()
+    override fun register(registerDto: RegisterDto) {
+        try {
+            var user: User = User()
             registerDtoToUser(registerDto, user)
 
-            if(checkExistId(user.id!!)){
-                throw HttpClientErrorException(HttpStatus.FORBIDDEN, "이미 존재하는 유저")
+            if (checkExistId(user.id!!)) {
+                throw HttpClientErrorException(HttpStatus.FORBIDDEN, "이미 존재하는 유저입니다.")
             }
 
             user.password = crypto.sha256(user.password!!)
 
             userRepo.save(user)
-        }catch (e: Exception){
+        } catch (e: Exception) {
             throw e;
         }
     }
 
     override fun checkId(id: String) {
-        if(checkExistId(id)){
-            throw HttpClientErrorException(HttpStatus.FORBIDDEN, "이미 존재하는 유저")
+        if (checkExistId(id)) {
+            throw HttpClientErrorException(HttpStatus.FORBIDDEN, "이미 존재하는 유저입니다.")
         }
     }
 
-    private fun checkExistId (id: String): Boolean {
+    override fun login(loginDto: LoginDto): LoginRo {
+        try {
+            checkExistIdAndPassword(loginDto.id!!, loginDto.password!!)
+
+            return makeJwt(loginDto.id!!)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    private fun checkExistIdAndPassword(id: String, password: String){
+        val findUser = userRepo.findById(id);
+        if(findUser == null){
+            throw HttpClientErrorException(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다.")
+        }
+
+        if(findUser.password != crypto.sha256(password)){
+            throw HttpClientErrorException(HttpStatus.BAD_REQUEST, "비밀번호가 다릅니다.")
+        }
+    }
+
+    private fun makeJwt(id: String): LoginRo{
+        val loginRo = LoginRo()
+        loginRo.accessToken = jwtService.createToken(id, JwtType.ACCESS)
+        loginRo.refreshToken = jwtService.createToken(id, JwtType.REFRESH)
+        return loginRo
+    }
+
+    private fun checkExistId(id: String): Boolean {
         var existUser = userRepo.findById(id)
         return (existUser != null)
     }
--- src/test/kotlin/com/map/map/serviceTest/auth/AuthExceptionTest.kt
+++ src/test/kotlin/com/map/map/serviceTest/auth/AuthExceptionTest.kt
@@ -1,15 +1,18 @@
 package com.map.map.serviceTest.auth
 
+import com.map.map.domain.dto.auth.LoginDto
 import com.map.map.domain.dto.auth.RegisterDto
 import com.map.map.enum.Gender
 import com.map.map.service.auth.AuthServiceImpl
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.client.HttpClientErrorException
 import java.util.*
 
-@SpringBootTest(classes = arrayOf(AuthServiceImpl::class))
+@SpringBootTest
 class AuthExceptionTest {
     @Autowired
     private lateinit var authServiceImpl: AuthServiceImpl
@@ -29,8 +32,9 @@ class AuthExceptionTest {
 
             authServiceImpl.register(registerDto)
             authServiceImpl.register(registerDto)
-        }catch (e: Exception){
+        }catch (e: HttpClientErrorException){
             e.printStackTrace()
+            assert(e.statusCode == HttpStatus.NOT_FOUND)
         }
     }
 
@@ -49,8 +53,63 @@ class AuthExceptionTest {
 
             var id = "aa"
             authServiceImpl.checkId(id)
-        }catch (e: Exception){
+        }catch (e: HttpClientErrorException){
             e.printStackTrace()
+            assert(e.statusCode == HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @Test
+    @Transactional
+    fun LoginNotFoundTest(){
+        try{
+            var registerDto: RegisterDto = RegisterDto()
+            registerDto.id= "qwe"
+            registerDto.password = "qwerqwer"
+            registerDto.gender = Gender.Male
+            registerDto.name = "qqqq"
+            registerDto.birthDate = Date()
+
+            authServiceImpl.register(registerDto)
+
+            var loginDto = LoginDto()
+            loginDto.id = "qe"
+            loginDto.password = "qwewqewqe"
+            authServiceImpl.login(loginDto)
+        }catch (e: HttpClientErrorException){
+            e.printStackTrace()
+            if(e.statusCode == HttpStatus.NOT_FOUND){
+                assert(e.statusCode == HttpStatus.NOT_FOUND)
+            }else{
+                throw e
+            }
+        }
+    }
+
+    @Test
+    @Transactional
+    fun LoginDifferentPasswordTest(){
+        try{
+            var registerDto: RegisterDto = RegisterDto()
+            registerDto.id= "qwe"
+            registerDto.password = "qwerqwer"
+            registerDto.gender = Gender.Male
+            registerDto.name = "qqqq"
+            registerDto.birthDate = Date()
+
+            authServiceImpl.register(registerDto)
+
+            var loginDto = LoginDto()
+            loginDto.id = "qwe"
+            loginDto.password = "qwewqewqe"
+            authServiceImpl.login(loginDto)
+        }catch (e: HttpClientErrorException){
+            e.printStackTrace()
+            if(e.statusCode == HttpStatus.BAD_REQUEST){
+                assert(e.statusCode == HttpStatus.BAD_REQUEST)
+            }else{
+                throw e
+            }
         }
     }
 }
