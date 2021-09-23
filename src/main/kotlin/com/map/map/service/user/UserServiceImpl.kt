3b552783d9c3c5ba84b9f9cf3343dc8b5f7f9508
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-09-23 14:51:40 +0900

    [add] delete user service

--- src/main/kotlin/com/map/map/service/user/UserServiceImpl.kt
+++ src/main/kotlin/com/map/map/service/user/UserServiceImpl.kt
@@ -1,21 +1,26 @@
 package com.map.map.service.user
 
+import com.map.map.domain.dto.user.DeleteUserDto
 import com.map.map.domain.dto.user.PatchUserBirthDateDto
 import com.map.map.domain.dto.user.PatchUserNameDto
 import com.map.map.domain.entity.User
 import com.map.map.domain.entity.backup.UserBackUp
 import com.map.map.domain.entity.backup.repo.UserBackUpRepo
 import com.map.map.domain.repository.UserRepo
+import com.map.map.lib.Crypto
 import com.map.map.service.auth.userToUserBackUp
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.client.HttpClientErrorException
 import java.util.*
 
 @Service
 class UserServiceImpl @Autowired constructor(
     private var userRepo: UserRepo,
-    private var userBackUpRepo : UserBackUpRepo
+    private var userBackUpRepo : UserBackUpRepo,
+    private var crypto: Crypto
 ) : UserService {
 
     /**
@@ -47,12 +52,41 @@ class UserServiceImpl @Autowired constructor(
     }
 
     /**
+     * 유저 삭제
+     */
+    @Transactional
+    override fun deleteUser(deleteUserDto: DeleteUserDto, user: User) {
+        try{
+            var password = crypto.sha256(deleteUserDto.password!!)
+
+            if(user.password!!.equals(password))
+                userRepo.deleteByIdx(user.idx!!)
+            else
+                throw HttpClientErrorException(HttpStatus.BAD_REQUEST, "비밀번호가 다릅니다.")
+
+            updateDeletedUserBackupData(user)
+        }catch (e : Exception){
+            throw e
+        }
+    }
+
+    /**
      * 유저 데이터 업데이트
      */
     private fun updateUserBackUpData(user: User){
         val userBackUp = UserBackUp()
         userToUserBackUp(user, userBackUp)
         userBackUp.updatedDate = Date()
+        userBackUpRepo.save(userBackUp)
+    }
+
+    /**
+     * 유저 삭제 데이터 업데이트
+     */
+    private fun updateDeletedUserBackupData(user: User){
+        val userBackUp = UserBackUp()
+        userToUserBackUp(user, userBackUp)
+        userBackUp.deletedDate = Date()
         userBackUpRepo.save(userBackUp)
     }
 
