1fe9da0088c1d8954e8142743083ad0a603670ba
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-10-02 21:49:22 +0900

    [add] spring security

---
+++ src/main/kotlin/com/map/map/config/SecurityConfig.kt
@@ -0,0 +1,39 @@
+package com.map.map.config
+
+import com.map.map.filter.JwtAuthenticationInterceptor
+import com.map.map.global.GlobalExceptionHandler
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    @Autowired
+    private var globalExceptionHandler : GlobalExceptionHandler? = null
+    @Autowired
+    private var jwtFilter : JwtAuthenticationInterceptor? = null
+    override fun configure(web: WebSecurity) {
+        web.ignoring()
+            .antMatchers("/file","file/**")
+            .antMatchers("/auth","auth/**")
+            .antMatchers("/swagger-ui.html")
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+    }
+
+    override fun configure(http: HttpSecurity?){
+        http!!
+            .httpBasic().disable()
+            .cors()
+            .and()
+            .csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/user","/user/**").permitAll()
+                .antMatchers("/album","album/**").permitAll()
+            .and()
+    }
+}
