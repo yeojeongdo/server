5c1a6127b15dfccf8c2b2589d4578d81ac01c3db
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-10-07 17:49:58 +0900

    [add] querydsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.5.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id ("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.spring") version "1.5.21"
     kotlin("plugin.jpa") version "1.5.21"
+    kotlin("kapt") version "1.3.61"
 }
 
 group = "com.map"
@@ -29,7 +31,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.hibernate:hibernate-validator:6.1.5.Final")
-
+    implementation ("com.querydsl:querydsl-jpa:4.4.0")
     implementation("io.springfox:springfox-swagger2:2.9.2")
     implementation("io.springfox:springfox-swagger-ui:2.9.2")
 
@@ -40,6 +42,10 @@ dependencies {
     implementation ("org.projectlombok:lombok")
 
     implementation("org.springframework.boot:spring-boot-starter-aop")
+
+
+    kapt("com.querydsl:querydsl-apt:4.4.0:jpa")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 
     runtimeOnly("mysql:mysql-connector-java")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -56,4 +62,9 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+//querydsl 추가 시작
+sourceSets["main"].withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class){
+    kotlin.srcDir("$buildDir/generated/source/kapt/main")
 }
