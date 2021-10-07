c3f3e3c31bf86c915dca6bc4b0ae3bbd6150c148
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-10-07 17:50:28 +0900

    [remvoe] albumlistrepo

--- src/main/kotlin/com/map/map/domain/repository/paging/AlbumListRepo.kt
+++
@@ -1,8 +0,0 @@
-package com.map.map.domain.repository.paging
-
-import com.map.map.domain.entity.Album
-import org.springframework.data.repository.PagingAndSortingRepository
-
-interface AlbumListRepo : PagingAndSortingRepository<Album, Long> {
-
-}
